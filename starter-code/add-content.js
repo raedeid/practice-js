0665bf365416f90ca4e318f00d2419f8d57b6388
raedeid <raededialrfou12345@gmail.com>
2019-11-06 17:21:13 +0200

    lab 07

--- starter-code/add-content.js
+++ starter-code/add-content.js
@@ -1,16 +1,27 @@
 
-var Age_of_customer = prompt("how old are you?");
+var Age_of_customer = prompt("how old are you? we have prize for the lucky number");
 var Username_of_customer = prompt("whats your name");
-function Information(Age , user){
+function Information(Age, user) {
     if (Age > 18 && Age < 25) {
-        greeting= 'Hi youn man :' + user;
+        greeting = ' Hi youn man :' + user;
     } else if (Age < 18) {
         greeting = ' Hi kid :' + user;
-    } else if (hourNow > 25) {
-        greeting = 'Hi man :' + user ;
+    } else if (Age > 25) {
+        greeting = ' Hi man :' + user;
     }
-    return greeting    
+    return greeting
+}
+function LuckyNumber() {
+    var Age_of_customer = prompt("how old are you? we have prize for the lucky number");
+    if (Age_of_customer == 33) {
+        Result = ' its your lucky day, congratulation ' + Username_of_customer;
+    } else {
+        Result = ' Sorry , hrad luck ';
     }
-document.write( '<h3>' + Information(Age_of_customer,Username_of_customer) + '</h3>'); 
+    return Result    
+}
+
+
+document.write('<h3>' + Information(Age_of_customer, Username_of_customer) + LuckyNumber() + '</h3>');
 
 
