367f59983a85f613bea125e33a79a03da1a7c392
raedeid <raededialrfou12345@gmail.com>
2019-11-07 00:23:26 +0200

    added picture

--- starter-code/add-content.js
+++ starter-code/add-content.js
@@ -1,11 +1,10 @@
 
-var Age_of_customer = prompt("how old are you? we have prize for the lucky number");
-var number_of_thing = prompt("how many thing you want to buy ?")
 
 function shopping(){
-    while (isNaN(number_of_thing)  || Age_of_customer == '' ){
-        var Age_of_customer = prompt("how old are you? we have prize for the lucky number");
-        number_of_thing += 1 ;
+    var number_of_thing = prompt("how many thing you want to buy ?");
+    while (isNaN(number_of_thing)  || number_of_thing == '' ){
+        var number_of_thing = prompt("how many thing you want to buy ?");
+    
 }
     for( var i=0; i<number_of_thing;i=i+1){
         document.write('<img src="'+'images/hotel.png' +'"/>');
