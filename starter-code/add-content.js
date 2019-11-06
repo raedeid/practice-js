c69de7f7570d16c9b21716e7a737b401a97adae2
raedeid <raededialrfou12345@gmail.com>
2019-11-06 13:02:52 +0200

    put another condation

---
+++ starter-code/add-content.js
@@ -0,0 +1,14 @@
+
+var Age_of_customer = prompt("how old are you?");
+var Username_of_customer = prompt("whats your name");
+function Information(Age , user){
+    if (Age > 18 && Age < 25) {
+        greeting= 'Hi youn man :' + user;
+    } else if (Age < 18) {
+        greeting = ' Hi kid :' + user;
+    } else if (hourNow > 25) {
+        greeting = 'Hi man :' + user ;
+    }
+    return greeting    
+    }
+document.write( '<h3>' + Information(Age_of_customer,Username_of_customer) + '</h3>'); 
